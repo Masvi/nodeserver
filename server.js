48b2c4a4de45605928fd610641337a1fdd995d40
Masvi <jeanmasvi@gmail.com>
2019-01-08 10:22:03 -0200

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-/*
-    Servidor HTTP NODEJS em JS puro que responde a 3 rotas de um site.
-    
-    0. Baixar nodejs: https://nodejs.org/en/download
-    1. Instalar nodejs
-    2. Rodar o arquivo em um terminal node: node server.js
-    3. No browser acessar o endereço: localhost:3001
-*/
 
 //global é a keyword para criar variáveis globais no node
 global.hoje = new Date();
